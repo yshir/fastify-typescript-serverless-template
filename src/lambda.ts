95333b7842473fc85b26b2a42cad0e54f3de72cc
yshir <yshir0020@gmail.com>
2021-04-25 03:52:52 +0900

    fix: build

---
+++ src/lambda.ts
@@ -0,0 +1,8 @@
+import awsLambdaFastify from 'aws-lambda-fastify';
+
+import { createApp } from '@src/app';
+
+const app = createApp();
+const proxy = awsLambdaFastify(app);
+
+export default proxy;
